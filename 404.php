a5c188bf560a4b67dc2a8b5077cdc60283fa265e
Nimmerz <funeralfear@gmail.com>
2016-02-23 22:52:18 +0200

    add some work

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+get_header(); ?>
+<div class="container body">
+	<main class="pull-left">
+		<h1 class="danger">Page not found</h1>
+		<img src="http://www.gr-cdn.com/images/errors/monster.png" class="danger-img">
+	</main>
+	<?php get_template_part('sidebar'); ?>
+	<div class="clearfix"></div>
+</div>
+<?php
+get_footer();
+
+ ?>
